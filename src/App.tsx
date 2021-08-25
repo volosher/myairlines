b76b39ff827f95759209a8975fef3a794f7219b9
Volodymyr Sheremeta <volosher@gmail.com>
2021-08-25 23:31:49 +0300

    add bootstrap v-5

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
-
+import 'bootstrap/scss/bootstrap.scss'
 
 function App() {
   return (
     <div className="App">
-
+        <div className="alert alert-primary" role="alert">
+            A simple primary alert—check it out!
+        </div>
+        <button type="button" className="btn btn-primary">
+            Notifications <span className="badge bg-secondary">4</span>
+        </button>
     </div>
   );
 }

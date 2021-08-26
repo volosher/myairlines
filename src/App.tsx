a5fde5405b14045358f8c3d66df9c1b2e6cd7867
Volodymyr Sheremeta <volosher@gmail.com>
2021-08-26 10:47:48 +0300

    create CurrencySwitcher component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import 'bootstrap/scss/bootstrap.scss'
+import CurrencySwitcher from "./components/CurrencySwitcher/CurrencySwitcher";
 
 function App() {
   return (
     <div className="App">
-        <div className="alert alert-primary" role="alert">
-            A simple primary alert—check it out!
-        </div>
-        <button type="button" className="btn btn-primary">
-            Notifications <span className="badge bg-secondary">4</span>
-        </button>
+        <CurrencySwitcher/>
     </div>
   );
 }
---
+++ src/components/CurrencySwitcher/CurrencySwitcher.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const myStyle = {
+    width: '90px',
+    borderRadius: '20px',
+}
+
+const CurrencySwitcher = () => {
+    return (
+        <div className="container">
+            <select  className="form-select" style={myStyle} aria-label="Currency switcher">
+                <option value="USD" selected>USD</option>
+                <option value="EUR">EUR</option>
+                <option value="UAH">UAH</option>
+                <option value="BYN">BYN</option>
+            </select>
+        </div>
+    );
+};
+
+export default CurrencySwitcher;

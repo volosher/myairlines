44e5f6dda877af6de323fa69780bb95e8a3c1068
Volodymyr Sheremeta <41182502+volosher@users.noreply.github.com>
2021-08-31 09:33:29 +0300

    customize export default preffer

--- src/components/UI/input/Input.tsx
+++ src/components/UI/input/Input.tsx
@@ -6,7 +6,6 @@ interface InputPlaceholder {
     text: string,
 }
 
-// eslint-disable-next-line import/prefer-default-export
 export const Input: React.FC<InputPlaceholder> = ({ text }: InputPlaceholder) => (
   <div className="search-bar__item">
     <input type="text" className="form-control search-bar__input" placeholder={text} aria-label="First name" />
